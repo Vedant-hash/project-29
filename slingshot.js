0a06628c650695fdfc61f8cc77ed8eea03062737
Vedant-hash <82369951+Vedant-hash@users.noreply.github.com>
2021-07-29 18:44:02 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,15 @@
+class SlingShot{
+    constructor(x,y){
+
+        this.slingShot=loadImage("polygon.png")
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.slingShot = null;
+    }
+
+ 
+    
+}
